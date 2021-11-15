9307d898e93383af850cf8308885ad3f9df2ad6c
dev-vicki <vignesh.r1999@gmail.com>
2021-11-16 02:15:23 +0530

    app-data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
+import data from "./util";
 
 function App() {
   return (
