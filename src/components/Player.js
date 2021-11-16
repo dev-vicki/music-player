43a55c9a45c5af573a26a269f2ec6b813a1e1f8d
dev-vicki <vignesh.r1999@gmail.com>
2021-11-16 16:15:53 +0530

    pause-play-with-icon-working.

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,6 @@
 import React, {useRef, useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faAngleLeft, faAngleRight}from "@fortawesome/free-solid-svg-icons";
+import { faPlay, faAngleLeft, faAngleRight, faPause}from "@fortawesome/free-solid-svg-icons";
 
 const Player = ({currentSong, isPlaying, setIsPlaying}) => {
   const audioRef = useRef(null);
@@ -41,8 +41,8 @@ return (
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x"icon={faAngleLeft} />
-        <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x"icon={faPlay} />
-        <FontAwesomeIcon className="skip-forward" size="2x"icon={faAngleRight} />
+        <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={isPlaying ? faPause : faPlay} />
+        <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
 
       </div>
       <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
