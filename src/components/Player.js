5929503c49c202ccf97a0a0f8f58af1edd4c49fc
dev-vicki <vignesh.r1999@gmail.com>
2021-11-22 13:58:35 +0530

    customizing user inputs

--- src/components/Player.js
+++ src/components/Player.js
@@ -52,10 +52,15 @@ if(direction === "skip-forward"){
   }
   playAudio(isPlaying, audioRef);
   };
+  // adding styles to track
+  const trackAnim = {
+    transform: `translateX(${songInfo.animationPercentage}%)`
+  }
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
+        <div style={{background: `linear-gradient(to right, ${currentSong.color[0]},${currentSong.color[1]})`}} className="track">
         <input
           min={0}
           max={songInfo.duration || 0}
@@ -63,6 +68,8 @@ if(direction === "skip-forward"){
           onChange={dragHandler}
           type="range"
         />
+        <div style={trackAnim} className="animate-track"></div>
+        </div>
         <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
       </div>
       <div className="play-control">
